33fe33cdf847d120b9d27ccff3c0672bfb2a65d7
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-20 17:19:22 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
+Da lì parte un timer di 30 secondi.
+Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+// Cosa serve: 
+// # Array vuoto in cui verranno inseriti i numeri casuali;
+// # Ciclo for per creare i box;
+// # Ciclo While per generare i numeri causali non ripetuti;
+// # metodo .push() per inseire i numeri dentro l+array
+// # funzione timeout per inserire timer di 30 sec
+// # prompt o casella input per l'utente
+// # condizione if per stabilire se i numeri utente combaciano con i numeri generati
